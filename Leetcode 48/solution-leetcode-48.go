c440fe592d17e900988e078509e16ab4aff51d25
vinhanhhaduc <vinhanh.haduc@gmail.com>
2024-07-17 00:07:53 +0700

    leetcode 48 solution

---
+++ Leetcode 48/solution-leetcode-48.go
@@ -0,0 +1,14 @@
+func rotate(matrix [][]int)  {
+    n := len(matrix)
+    for i := 0; i < n; i++ {
+        for j := i; j < n; j++ {
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        }
+    }
+
+    for i := 0; i < n; i++ {
+        for j := 0; j < n/2; j++ {
+            matrix[i][j], matrix[i][n- 1 - j] = matrix[i][n - 1 - j], matrix[i][j]
+        }
+    }
+}
